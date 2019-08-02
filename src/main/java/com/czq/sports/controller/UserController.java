894d3497fe757d71ee89c16d8c9bf84edff57290
czqiang_1@163.com <czqiang_1@163.com>
2019-08-02 17:35:34 +0800

    1、添加get_info后台接口

--- src/main/java/com/czq/sports/controller/UserController.java
+++ src/main/java/com/czq/sports/controller/UserController.java
@@ -7,10 +7,13 @@ import com.czq.sports.utils.ResultCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -28,12 +31,30 @@ public class UserController {
         User user = userService.getUserByAccount(userName);
         if (user == null) {
             logger.error("用户不存在");
-            result.setCode(ResultCode.NOTUSER);
             result.setMsg("用户不存在");
-        } else {
-
+            result.setCode(ResultCode.NOTUSER);
         }
+        return result;
+    }
 
+    @GetMapping("/get_info")
+    public BaseResult getInfo() {
+        BaseResult result = new BaseResult();
+        /*
+            name: 'super_admin',
+            user_id: '1',
+            access: ['super_admin', 'admin'],
+            token: 'super_admin',
+            avator: 'https://file.iviewui.com/dist/a0e88e83800f138b94d2414621bd9704.png'
+        */
+        Map<String, Object> data = new HashMap<>(5);
+        data.put("name", "admin");
+        data.put("user_id", 1);
+        data.put("access", new String[]{"super_admin", "admin"});
+        data.put("token", "super_admin");
+        data.put("avator", "https://file.iviewui.com/dist/a0e88e83800f138b94d2414621bd9704.png");
+
+        result.setData(data);
         return result;
     }
 }
