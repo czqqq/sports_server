e7c8d86e90f996cc001feee4085d371bb70c01b2
czqiang_1@163.com <czqiang_1@163.com>
2019-08-20 17:51:44 +0800

    上传excel接口开发

--- src/main/java/com/czq/sports/controller/ExcelController.java
+++ src/main/java/com/czq/sports/controller/ExcelController.java
@@ -1,17 +1,17 @@
 package com.czq.sports.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/upload")
 public class ExcelController {
 
     @RequestMapping("uploadExcel")
-    public void uploadExcel(@RequestParam("fileName") MultipartFile file){
-        if(file.isEmpty()){
-            System.out.println("错误");
+    public void uploadExcel(MultipartFile file){
+        if(file == null){
+            System.out.println("上传excel错误，文件为空");
         }
     }
 
