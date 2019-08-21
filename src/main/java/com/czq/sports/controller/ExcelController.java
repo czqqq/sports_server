bd31f002895b11e758b4ceeeee1b1fc785db6206
czqiang_1@163.com <czqiang_1@163.com>
2019-08-21 15:20:28 +0800

    1上传excel接口bug修改 2添加 alibaba easyExcel 依赖

--- src/main/java/com/czq/sports/controller/ExcelController.java
+++ src/main/java/com/czq/sports/controller/ExcelController.java
@@ -1,6 +1,8 @@
 package com.czq.sports.controller;
 
+import com.czq.sports.utils.BaseResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,10 +11,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class ExcelController {
 
     @RequestMapping("uploadExcel")
-    public void uploadExcel(MultipartFile file){
+    public BaseResult uploadExcel(@RequestParam("file") MultipartFile file){
         if(file == null){
             System.out.println("上传excel错误，文件为空");
         }
+
+
+        return new BaseResult();
     }
 
     
